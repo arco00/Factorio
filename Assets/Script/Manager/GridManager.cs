6a3093c6f8012a7cc299ec002d29722838f5dfb9
arco00 <enkilorien@gmail.com>
2024-07-25 11:37:49 +0200

    Update GridManager.cs

--- Assets/Script/Manager/GridManager.cs
+++ Assets/Script/Manager/GridManager.cs
@@ -2,11 +2,13 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class GridManager : Singleton<GridManager>
 {
     //public class UIManager : Singleton<UIManager>
-    [SerializeField] List<TilemapSlot> listTilemap = new List<TilemapSlot>();
+    //[SerializeField] List<TilemapSlot> listTilemap = new List<TilemapSlot>(); passe sur un dictionary
+    Dictionary<Vector2, BaseObject> TilemapDictionary;
     [SerializeField] protected Grid grid = null;
     // Start is called before the first frame update
     public Grid Grid => grid;   
@@ -19,15 +21,21 @@ public class GridManager : Singleton<GridManager>
     {
         return grid.WorldToCell(_pos);
     }
+    public Vector2Int GetGridPos2D(Vector3 _pos)
+    {
+        Vector3Int _posInt = grid.WorldToCell(_pos);
+        return new Vector2Int(_posInt.x,_posInt.y);
+    }
     public  bool SetGridPosAt(Vector2Int _pos,BaseObject _gameObject)
     {
         //tester si la case est libre 
-      
-        if (PosTaken(_pos,out TilemapSlot _result ))return false;
 
-        listTilemap.Add(new TilemapSlot(_pos,_gameObject));
+        if (TilemapDictionary.TryAdd(_pos, _gameObject))
+        {
         _gameObject.transform.position=grid.CellToWorld(new Vector3Int(_pos.x,_pos.y,0));
         return true;
+        }
+        return false;
     }
 
     public bool SetGridPosAt(Vector3Int _pos, BaseObject _gameObject)
@@ -35,45 +43,61 @@ public class GridManager : Singleton<GridManager>
         //tester si la case est libre 
 
         Vector2Int _pos2D = new Vector2Int(_pos.x, _pos.y);
-        if (PosTaken(_pos2D, out TilemapSlot _result)) return false;
-
-        listTilemap.Add(new TilemapSlot(_pos2D, _gameObject));
-        _gameObject.transform.position = grid.CellToWorld(new Vector3Int(_pos.x, _pos.y, 0));
-        return true;
+        if (TilemapDictionary.TryAdd(_pos2D, _gameObject))
+        {
+            _gameObject.transform.position = grid.CellToWorld(new Vector3Int(_pos.x, _pos.y, 0));
+            return true;
+        }
+        return false;
     }
     public bool ObjectOfTypeAtPos(Vector2Int _pos,string _class)
     {
+        //1er test sans le dictionary
         // know if ther is an object of type class at pos
         //changer la 2eme var
         //pas de string
         //Debug.Log(_class);
-        if (PosTaken(_pos, out TilemapSlot _result))
+        //if (PosTaken(_pos, out TilemapSlot result))
+        //{
+        //    // Debug.Log(_result.gameObject.GetComponent(_class).name.ToString());
+        //    //pas la bonne comparaison 
+        //    bool _bool = _result.BaseObject.GetComponent(_class) != null;
+        //    //Debug.Log(_bool);
+        //    //Debug.Log(_result.GameObject.GetComponent<BaseObject>().ToString());
+        //    return _bool ;
+        //}
+        //return false;
+        if (TilemapDictionary.TryGetValue(_pos,out BaseObject _result ))
         {
-            // Debug.Log(_result.gameObject.GetComponent(_class).name.ToString());
-            //pas la bonne comparaison 
-            bool _bool = _result.BaseObject.GetComponent(_class) != null;
-            //Debug.Log(_bool);
-            //Debug.Log(_result.GameObject.GetComponent<BaseObject>().ToString());
-            return _bool ;
+            return _result.GetComponent(_class) ;  
         }
-        return false;
+        return false; 
+        ///mpmllp
+
+
     }
 
 
     public bool PosTaken(Vector2Int _pos, out TilemapSlot _result)
     {
 
-        foreach (TilemapSlot _slot in listTilemap)
-        {
-            if (_slot.Position == _pos)
-            {
-                //savoir par quoi (_slot) est prise la case
-                _result = _slot;
-                return true;
-            }
-        }
+        
+        //foreach (Key _slot in TilemapDictionary)
+        //{
+        //    if (_slot == _pos)
+        //    {
+        //        //savoir par quoi (_slot) est prise la case
+        //        _result = _slot;
+        //        return true;
+        //    }
+        //}
         _result = TilemapSlot.Null();
-        return false;
+        return TilemapDictionary.ContainsKey(_pos);
+    }
+
+    public bool PosTakenBy(Vector2Int _pos, out TilemapSlot _result)
+    {
+
     }
 
     public bool PosTaken(Vector3Int _pos3D, out TilemapSlot _result)
